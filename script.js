654d2ef99d6c2fb38750817d728cebec9171cda7
maria <saprina.mariya@mail.ru>
2020-06-10 00:11:22 +0300

    added first material

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', '');
+            const b = prompt('На сколько оцените его?', '');
+        
+            if ( a != null && b != null && a != '' && b != '' && a.length < 4) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+            
+        }
+    },
+
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('просмотрено мало');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('вы киноман');
+        } else {
+            console.log('Ошибка');
+        }    
+    },
+
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            const genre = prompt(`Ваш любимый жанр ${i}`);
+            personalMovieDB.genres[i - 1] = genre;
+        }
+    }
+};
+
